ed6f6189c112ecfe73eeb2e989e71ef725faf856
CahayaSpace <nurakbarsemarang1@gmail.com>
2024-10-31 19:33:14 +0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html}"],
+  content: ["index.html","kertas.html","pewarna.html","plastik.html"],
   theme: {
     extend: {},
+    backgroundImage: {
+      'recycle': "url('/src/asset/fixed.png')",
+    },
+
+    fontFamily: {
+     Poppins: ['Poppins'],
+    },
   },
   plugins: [],
 }
